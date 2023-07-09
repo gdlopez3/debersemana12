25cd7fe302f85b81446a155a39dbef6d17d09604
gdlopez3 <gdlopez3@utpl.edu.ec>
2023-07-08 21:36:22 -0500

    crear app.py

--- app.py
+++ app.py
@@ -6,28 +6,36 @@ import requests
 import time
 import csv
 import threading
-# librer√≠a de python que permite ejecutar comandos
 import subprocess
+import os 
+
+#Se obtienen las p†ginas web de los hoteles que existen en Atacames.
 
 def obtener_data():
     lista = []
-    with open("informacion/data.csv") as archivo:
-        lineas = csv.reader(archivo, quotechar="|")
+    with open("PAGINAS/hoteles.csv") as archivo:
+        lineas = csv.reader(archivo, delimiter='|')
+
         for row in lineas:
-            # pass
-            # lista.append((numero, pagina))
-    # se retorna la lista con la informaci√≥n que se necesita
+            numero = row[0].strip()
+            url = row[1].strip()
+            lista.append((numero, url))
     return lista
 
 def worker(numero, url):
-    print("Iniciando %s %s" % (threading.current_thread().getName(), url ))
-    # pass
+    print("Iniciando %s %s" % (threading.current_thread().getName(), url))
+    pagina = requests.get(url)
+    nombre_archivo = f"{numero}.txt"
+    ruta_archivo = os.path.join("SALIDA", nombre_archivo)
+    with open(ruta_archivo, "w") as archivo:
+        archivo.writelines(pagina.text)
+    print(f"Archivo guardado: {nombre_archivo}")
     time.sleep(10)
     print("Finalizando %s" % (threading.current_thread().getName()))
 
 for c in obtener_data():
     # Se crea los hilos
-    # en la funci√≥n
+    # en la funci¢n
     numero = c[0]
     url = c[1]
     hilo1 = threading.Thread(name='navegando...',
